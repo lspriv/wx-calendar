439ddaeaaff6f115d7007614b8fb2fdcd7b8db49
lspriv <976928310@qq.com>
2024-02-23 07:15:06 +0800

    build(package): version 1.6.1-alpha.2

--- src/lib.ts
+++ src/lib.ts
@@ -4,12 +4,12 @@
  * See File LICENSE for detail or copy at https://opensource.org/licenses/MIT
  * @Description: Description
  * @Author: lspriv
- * @LastEditTime: 2024-02-22 19:30:36
+ * @LastEditTime: 2024-02-23 07:14:32
  */
-export * from './utils/shared';
 export type * from './basic/tools';
 export type * from './interface/component';
 export type * from './basic/service';
+export * from './utils/shared';
 export * from './basic/constants';
 export { nextTick, severalTicks, viewFlag, flagView, isViewFixed, isSkyline } from './basic/tools';
 export * from './interface/calendar';
