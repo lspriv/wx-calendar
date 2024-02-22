da329d63897478327c215d25ac4f872a1609c96a
lspriv <976928310@qq.com>
2024-02-22 19:31:23 +0800

    fix(*): 导出Layout

--- src/lib.ts
+++ src/lib.ts
@@ -4,14 +4,15 @@
  * See File LICENSE for detail or copy at https://opensource.org/licenses/MIT
  * @Description: Description
  * @Author: lspriv
- * @LastEditTime: 2024-02-22 18:41:01
+ * @LastEditTime: 2024-02-22 19:30:36
  */
 export * from './utils/shared';
 export type * from './basic/tools';
 export type * from './interface/component';
 export type * from './basic/service';
-export * from './interface/calendar';
 export * from './basic/constants';
 export { nextTick, severalTicks, viewFlag, flagView, isViewFixed, isSkyline } from './basic/tools';
+export * from './interface/calendar';
+export { Layout } from './basic/layout';
 export { LUNAR_PLUGIN_KEY } from './plugins/lunar';
 export { MARK_PLUGIN_KEY } from './plugins/mark';
